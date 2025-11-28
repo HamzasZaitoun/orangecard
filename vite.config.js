ceef8f7140f6dd0726dbc2e12f5668a0aae0abb4
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2025-11-29 02:42:13 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-            // add your js entry if you have one:
-            // 'resources/js/app.js',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
