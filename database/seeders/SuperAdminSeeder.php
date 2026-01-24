b8e901a5abc9907b3e9c2a74b0a1350d0a4b1d56
Hamza Zaitoun <hamzasaadzaitoun@gmail.com>
2026-01-24 22:46:31 +0300

    fix

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -17,7 +17,7 @@ class SuperAdminSeeder extends Seeder
             'name' => 'Super Admin',
             'username' => 'superadmin',
             'email' => 'admin@eliteplusnfc.com',
-            'password' => Hash::make('Elite@2026!Secure'),
+            'password' => Hash::make('123456'),
             'user_role' => 'super_admin',
             'is_active' => true,
         ]);
@@ -25,7 +25,7 @@ class SuperAdminSeeder extends Seeder
             'name' => 'Super Admin',
             'username' => 'admin',
             'email' => 'admin@eliteplusnfc.com',
-            'password' => Hash::make('elite123'),
+            'password' => Hash::make('eliteadmin123$$'),
             'user_role' => 'admin',
             'is_active' => true,
         ]);
